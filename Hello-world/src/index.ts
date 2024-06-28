090574e0015d560d35900f8a2458a3f44f5d4fd4
zohredk <zohrehdk2000@gmail.com>
2024-06-29 08:09:03 +1000

    .

--- Hello-world/src/index.ts
+++ Hello-world/src/index.ts
@@ -1,3 +1,3 @@
-let sales: number = 123_456_789;
-let course: string = 'TypeScript';
-let is_published: boolean = true;
+let sales = 123_456_789;
+let course = 'TypeScript';
+let is_published = true;
